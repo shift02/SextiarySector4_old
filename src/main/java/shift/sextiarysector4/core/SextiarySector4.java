39382baf1b1cdff0f0709b22e58eecbfd9019279
shift02 <shift02ss@gmail.com>
2019-04-06 23:59:02 +0900

    Tomlファイルを編集

---
+++ src/main/java/shift/sextiarysector4/core/SextiarySector4.java
@@ -0,0 +1,10 @@
+package shift.sextiarysector4.core;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(SextiarySector4.MOD_ID)
+public class SextiarySector4 {
+
+    public static final String MOD_ID = "sextiarysector4";
+
+}

7bdac2a443de22a4be702fd51b8c2bcc828e179a
shift02 <shift02ss@gmail.com>
2019-05-02 23:01:41 +0900

    共通処理をLibに移動

--- src/main/java/shift/sextiarysector4/core/block/BlockTank.java
+++ src/main/java/shift/sextiarysector4/core/block/BlockTank.java
@@ -1,11 +1,7 @@
 package shift.sextiarysector4.core.block;
 
 
-import java.util.function.BiFunction;
-import java.util.function.Supplier;
-
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
@@ -29,13 +25,12 @@ import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import shift.sextiarysector4.core.tileentity.IFeatureBlock;
+import shift.sextiarysector4.lib.block.BlockSextiarySector4;
 
 /**
  * 液体を入れるタンク
  */
-public class BlockTank extends Block implements IFluidWaterBlock {
-
-    protected BiFunction<IBlockState, IBlockReader, TileEntity> createTileEntityFunction;
+public class BlockTank extends BlockSextiarySector4 implements IFluidWaterBlock {
 
     public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
 
@@ -49,27 +44,6 @@ public class BlockTank extends Block implements IFluidWaterBlock {
 
     protected void fillStateContainer(StateContainer.Builder<Block, IBlockState> builder) {
         builder.add(WATERLOGGED);
-    }
-
-    public Block setCreateTileEntityFunction(BiFunction<IBlockState, IBlockReader, TileEntity> createTileEntityFunction) {
-        this.createTileEntityFunction = createTileEntityFunction;
-        return this;
-    }
-
-    public Block setCreateTileEntityFunction(Supplier<TileEntity> createTileEntityFunction) {
-        this.createTileEntityFunction = (s, r) -> createTileEntityFunction.get();
-        return this;
-    }
-
-    @Deprecated
-    public boolean onBlockActivated(IBlockState state, World worldIn, BlockPos pos, EntityPlayer player, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ) {
-        TileEntity tileEntity = worldIn.getTileEntity(pos);
-
-        if (tileEntity instanceof IFeatureBlock) {
-            return ((IFeatureBlock) tileEntity).onBlockActivated(state, worldIn, pos, player, hand, side, hitX, hitY, hitZ);
-        }
-
-        return false;
     }
 
     public boolean propagatesSkylightDown(IBlockState state, IBlockReader reader, BlockPos pos) {
@@ -109,17 +83,6 @@ public class BlockTank extends Block implements IFluidWaterBlock {
     @Override
     public boolean isFullCube(IBlockState state) {
         return false;
-    }
-
-    @Override
-    public boolean hasTileEntity(IBlockState state) {
-        return createTileEntityFunction != null;
-    }
-
-    @Nullable
-    @Override
-    public TileEntity createTileEntity(IBlockState state, IBlockReader world) {
-        return createTileEntityFunction.apply(state, world);
     }
 
     //液体関係
---
+++ src/main/java/shift/sextiarysector4/lib/SSLib.java
@@ -0,0 +1,10 @@
+package shift.sextiarysector4.lib;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(SSLib.MOD_ID)
+public class SSLib {
+
+    public static final String MOD_ID = "sextiarysector4lib";
+
+}
